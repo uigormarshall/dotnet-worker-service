1880b69c0a31b7b3ce38a79a21dbc44ede8ce4e0
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-18 11:42:59 -0300

    Add simple test to check if sites are working

---
+++ worker-log/worker-log/Entities/CompanyEntitity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace worker_log.Entities
+{
+    public class CompanyEntitity
+    {
+        public CompanyEntitity(string name, string uri)
+        {
+            Name = name;
+            Uri = new Uri(uri);
+        }
+        public string Name { get; set; }
+        public Uri Uri { get; set; }
+    }
+}
--- worker-log/worker-log/Worker.cs
+++ worker-log/worker-log/Worker.cs
@@ -1,20 +1,27 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using worker_log.Entities;
 
 namespace worker_log
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private  IList<CompanyEntitity> _companies;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            _companies = new List<CompanyEntitity>();
+            mockCompanies();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -22,8 +29,40 @@ namespace worker_log
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+
+
+                foreach (var company in _companies)
+                {
+                    var ping = IsConectedToHost(company.Uri);
+                    var strStatus = ping == true ? "Okay" : "Problem";
+                    var timeNow = DateTimeOffset.Now;
+                    _logger.LogInformation($"{strStatus} with {company.Name} on: {timeNow}");
+                }
+
+                await Task.Delay(10000, stoppingToken);
             }
+        }
+
+        public static bool IsConectedToHost(Uri uri)
+        {
+            try
+            {
+                Dns.GetHostEntry(uri.Host);
+                return true;
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+        }
+
+        private void mockCompanies()
+        {
+            _companies.Add(new CompanyEntitity("Google", "http://google.com"));
+            _companies.Add(new CompanyEntitity("Interisk Site", "https://www.brasiliano.com.br/"));
+            _companies.Add(new CompanyEntitity("Interisk Dashboard", "http://google.com"));
+            _companies.Add(new CompanyEntitity("Namespace", "http://namespace.net.br"));
+
         }
     }
 }
